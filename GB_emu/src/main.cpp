5f0520c3f82918864742107c49d8be041196f24a
Olivier Chabloz <olivier.chabloz@sensima.ch>
2017-10-18 15:20:06 +0200

    remove sdl dependency from everything but the main

--- GB_emu/src/main.cpp
+++ GB_emu/src/main.cpp
@@ -24,20 +24,37 @@
 #include <getopt.h>
 
 
+#define BGB
+
+#ifdef BGB // BGB palette (greenish tint)
+#define COLOR0 0xe0f8d0
+#define COLOR1 0x88c070
+#define COLOR2 0x346856
+#define COLOR3 0x081820
+#else      // B/W palette
+#define COLOR0 0xEEEEEE
+#define COLOR1 0xCCCCCC
+#define COLOR2 0x888888
+#define COLOR3 0x000000
+#endif
 
 
 void audio_callback(void * data, uint8_t * stream, int len);
 
-int main(int argc, char * const argv[]) {
+#define MAP_COLOR(X) SDL_MapRGBA(screen->format,(X & 0xff0000)>>16, (X & 0xff00)>>8,X & 0xff, 0xff)
+
+int main(int argc, char * argv[]) {
     double screen_scale = 4;
     int screen_width = ceilf(160.0 * screen_scale);
     int screen_height = ceilf(144.0 * screen_scale);
     bool fullscreen = false;
+    bool disable_audio = false;
     int c;
     char * cart_name = nullptr;
     char * boot_rom  = nullptr;
+    SDL_Surface * screen;
     // parse arguments :
-    while ((c = getopt(argc, argv, "fr:b:s:")) != -1) {
+    while ((c = getopt(argc, argv, "fr:b:s:a")) != -1) {
         switch (c) {
             case 'f':
                 fullscreen = true;
@@ -55,16 +72,18 @@ int main(int argc, char * const argv[]) {
                 break;
             case 'b':
                 boot_rom = optarg;
+            case 'a':
+                disable_audio = true;
             default:
                 break;
         }
     }
-    
+
     if (cart_name == nullptr) {
         printf("Usage : gb_emu -r romfile.gb\n");
         return EXIT_SUCCESS;
     }
-    
+
     Cart cart(cart_name);
     if(!cart.status()){
         printf("the cartridge header doesn't match the checksum\n");
@@ -74,10 +93,12 @@ int main(int argc, char * const argv[]) {
         printf("wrong rom file\n");
         return EXIT_FAILURE;
     }
-    
     // initialize gameboy peripherals
-    
-    PPU ppu(cart.status());
+	//boot_rom = "C:\\Users\\ochab\\Dropbox\\Divers\\bios\\gb_bios.bin";
+	//Cart cart("C:\\Users\\ochab\\Dropbox\\Divers\\roms\\Game Boy\\Metroid II - Return of Samus (World).gb");
+
+    screen = SDL_CreateRGBSurface(0, 160, 144, 32, 0, 0, 0, 0);
+    PPU ppu(cart.status(), (uint32_t *)screen->pixels, COLOR0, COLOR1, COLOR2, COLOR3);
     APU apu;
     Memory_map *memory;
     Cpu * processor;
@@ -89,25 +110,26 @@ int main(int argc, char * const argv[]) {
         memory = new Memory_map(&cart, &ppu, &apu, boot_rom);
         processor = new Cpu(memory, true);
     }
-    
+
     // initialise audio
-    
+
     static SDL_AudioSpec audio_spec;
     audio_spec.format = AUDIO_S16;
     audio_spec.callback = audio_callback;
     audio_spec.userdata = &apu;
     audio_spec.freq = 44100;
     audio_spec.samples = 2048;
-    if (SDL_OpenAudio(&audio_spec, NULL) < 0) {
-        fprintf(stderr, "Cannot open audio: %s", SDL_GetError());
-        return EXIT_FAILURE;
+    if(!disable_audio){
+        if (SDL_OpenAudio(&audio_spec, NULL) < 0) {
+            fprintf(stderr, "Cannot open audio: %s", SDL_GetError());
+            return EXIT_FAILURE;
+        }
     }
-    
     // Initialize Screen
-    
+
     SDL_Window* window = NULL;
     SDL_Surface* screenSurface = NULL;
-    
+
     //Initialize SDL
     if( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO)< 0 ){
         return EXIT_FAILURE;
@@ -120,13 +142,13 @@ int main(int argc, char * const argv[]) {
                               SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE |
                               ((fullscreen) ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0)
                               );
-    
+
     screenSurface = SDL_GetWindowSurface(window);
-    
-    
+
+
     uint32_t time = SDL_GetTicks();
     bool quit = false;
-    
+
     uint64_t cycles = 0;
     SDL_PauseAudio(0);
     while(!quit){
@@ -162,11 +184,11 @@ int main(int argc, char * const argv[]) {
                     break;
                 }
                 case SDL_QUIT: quit = true; break;
-                
+
                 default: break;
             }
         }
-        
+
         cycles = 0;
         uint32_t delta = (SDL_GetTicks() - time);
         if(delta < 13){
@@ -175,7 +197,7 @@ int main(int argc, char * const argv[]) {
         {
             //Apply the image stretched
             SDL_Rect stretchRect;
-            
+
             stretchRect.y = 0;
             SDL_GetWindowSize(window, &stretchRect.w, &stretchRect.h);
             screenSurface = SDL_GetWindowSurface(window);
@@ -184,7 +206,8 @@ int main(int argc, char * const argv[]) {
             stretchRect.w = scale * 160.0;
             stretchRect.x = (stretchRect.x - stretchRect.w) / 2;
             stretchRect.x = (stretchRect.x < 0) ? 0 : stretchRect.x;
-            SDL_BlitScaled(ppu.get_screen(), NULL, screenSurface, &stretchRect );
+            ppu.get_screen();
+            SDL_BlitScaled(screen, NULL, screenSurface, &stretchRect );
             SDL_UpdateWindowSurface(window);
         }
     }
