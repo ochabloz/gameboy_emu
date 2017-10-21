a11565c38841d935d8717f49d01692fb5effe180
Olivier Chabloz <olivier.chabloz@sensima.ch>
2017-10-21 10:10:34 +0200

    main is now using home made argparse

--- GB_emu/src/main.cpp
+++ GB_emu/src/main.cpp
@@ -1,4 +1,3 @@
-
 //
 //  main.cpp
 //  GB_emu
@@ -7,8 +6,8 @@
 //  Copyright © 2017 Olivier Chabloz. All rights reserved.
 //
 
+#include <stdlib.h>
 #include <iostream>
-
 #include <vector>
 
 #include "Cart.hpp"
@@ -16,12 +15,16 @@
 #include "Cpu.hpp"
 #include "PPU.hpp"
 #include "APU.hpp"
+
+extern "C"{
+  #include "argparse.h"
+}
+
+#ifndef _WIN32
 #include <SDL2/SDL.h>
-
-#include <stdlib.h>
-#include <unistd.h>
-
-#include <getopt.h>
+#else
+#include <SDL.h>
+#endif
 
 
 #define BGB
@@ -47,40 +50,28 @@ int main(int argc, char * argv[]) {
     double screen_scale = 4;
     int screen_width = ceilf(160.0 * screen_scale);
     int screen_height = ceilf(144.0 * screen_scale);
+
     bool fullscreen = false;
-    bool disable_audio = false;
-    int c;
-    char * cart_name = nullptr;
-    char * boot_rom  = nullptr;
+    bool disable_audio = true;
+
     SDL_Surface * screen;
     // parse arguments :
-    while ((c = getopt(argc, argv, "fr:b:s:a")) != -1) {
-        switch (c) {
-            case 'f':
-                fullscreen = true;
-                break;
-            case 's':
-                if (atof(optarg) > 0.1) {
-                    screen_scale = atof(optarg);
-                    screen_width = 160.0 * screen_scale;
-                    screen_height = 144.0 * screen_scale;
-                }
-                else printf("-s %s is not a valid argument\n", optarg);
-                break;
-            case 'r':
-                cart_name = optarg;
-                break;
-            case 'b':
-                boot_rom = optarg;
-            case 'a':
-                disable_audio = true;
-            default:
-                break;
+    argparse_t parser = argparse_init(argc, (const char **)argv);
+    const char * boot_rom  = argparse_get_opt(parser, 'b');
+    const char * cart_name = argparse_get_positional(parser, 0);
+    const char * scale = argparse_get_opt(parser, 's');
+
+    if(scale != nullptr){
+        if (atof(scale) > 0.1) {
+            screen_scale = atof(scale);
+            screen_width = 160.0 * screen_scale;
+            screen_height = 144.0 * screen_scale;
         }
+        else printf("-s %s is not a valid argument\n", scale);
     }
 
     if (cart_name == nullptr) {
-        printf("Usage : gb_emu -r romfile.gb\n");
+        printf("Usage : gb_emu romfile.gb\n");
         return EXIT_SUCCESS;
     }
 
@@ -93,12 +84,16 @@ int main(int argc, char * argv[]) {
         printf("wrong rom file\n");
         return EXIT_FAILURE;
     }
-    // initialize gameboy peripherals
-	//boot_rom = "C:\\Users\\ochab\\Dropbox\\Divers\\bios\\gb_bios.bin";
-	//Cart cart("C:\\Users\\ochab\\Dropbox\\Divers\\roms\\Game Boy\\Metroid II - Return of Samus (World).gb");
 
     screen = SDL_CreateRGBSurface(0, 160, 144, 32, 0, 0, 0, 0);
-    PPU ppu(cart.status(), (uint32_t *)screen->pixels, COLOR0, COLOR1, COLOR2, COLOR3);
+    PPU ppu(cart.status(),
+            (uint32_t *)screen->pixels,
+            MAP_COLOR(COLOR0),
+            MAP_COLOR(COLOR1),
+            MAP_COLOR(COLOR2),
+            MAP_COLOR(COLOR3)
+          );
+
     APU apu;
     Memory_map *memory;
     Cpu * processor;
@@ -112,7 +107,6 @@ int main(int argc, char * argv[]) {
     }
 
     // initialise audio
-
     static SDL_AudioSpec audio_spec;
     audio_spec.format = AUDIO_S16;
     audio_spec.callback = audio_callback;
@@ -121,38 +115,37 @@ int main(int argc, char * argv[]) {
     audio_spec.samples = 2048;
     if(!disable_audio){
         if (SDL_OpenAudio(&audio_spec, NULL) < 0) {
-            fprintf(stderr, "Cannot open audio: %s", SDL_GetError());
+            fprintf(stderr, "Cannot open audio: %s\n", SDL_GetError());
             return EXIT_FAILURE;
         }
     }
     // Initialize Screen
 
     SDL_Window* window = NULL;
-    SDL_Surface* screenSurface = NULL;
 
     //Initialize SDL
     if( SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO)< 0 ){
+		fprintf(stderr, "Cannot init SDL: %s\n", SDL_GetError());
         return EXIT_FAILURE;
     }
     char title[16];
     cart.get_title(title);
-    window = SDL_CreateWindow( title, SDL_WINDOWPOS_CENTERED,
-                                      SDL_WINDOWPOS_CENTERED,
-                              screen_width, screen_height,
+    window = SDL_CreateWindow(title,
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              screen_width,
+                              screen_height,
                               SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE |
                               ((fullscreen) ? SDL_WINDOW_FULLSCREEN_DESKTOP : 0)
                               );
 
-    screenSurface = SDL_GetWindowSurface(window);
 
-
-    uint32_t time = SDL_GetTicks();
     bool quit = false;
-
     uint64_t cycles = 0;
     SDL_PauseAudio(0);
+
     while(!quit){
-        time = SDL_GetTicks();
+        uint32_t time = SDL_GetTicks();
         while (!ppu.screen_complete) {
             cycles += processor->run();
         }
@@ -194,13 +187,13 @@ int main(int argc, char * argv[]) {
         if(delta < 13){
             SDL_Delay(13 - delta);
         }
+        
+        //Apply the image stretched
         {
-            //Apply the image stretched
             SDL_Rect stretchRect;
-
             stretchRect.y = 0;
             SDL_GetWindowSize(window, &stretchRect.w, &stretchRect.h);
-            screenSurface = SDL_GetWindowSurface(window);
+            SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
             float scale = floor(stretchRect.h / 140.0 * 10.0) / 10.0;
             stretchRect.x = stretchRect.w;
             stretchRect.w = scale * 160.0;
