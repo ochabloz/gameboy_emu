afcc9582f32f335ac6ac3a7f83055d20066a49b6
Olivier CHABLOZ <ochabloz@gmail.com>
2019-01-03 12:55:40 +0100

    will test differents path depending on the testing os

--- tests/test_utils.c
+++ tests/test_utils.c
@@ -6,6 +6,15 @@ extern "C"{
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef __unix__  
+#define PATH_TEST1 "/Users/georges/Downloads/"
+#define PATH_TEST11 "/Users/georges/Downloads"
+#define PATH_FULL "/Users/georges/Downloads/my-file.ext"
+#else
+#define PATH_TEST1 "C:\\Users\\georges\\Downloads\\"
+#define PATH_TEST11 "C:\\Users\\georges\\Downloads"
+#define PATH_FULL "C:\\Users\\georges\\Downloads\\my-file.ext"
+#endif
 
 TEST_GROUP(test_utils){};
 
@@ -41,13 +50,13 @@ TEST(test_utils, replace_with_shorter_ext){
 }
 
 TEST(test_utils, get_filename){
-    const char * path = "C:\\Users\\georges\\Downloads\\my-file.ext";
+    const char * path = PATH_FULL;
     const char * filename = path_get_filename(path);
     STRCMP_EQUAL("my-file.ext", filename);
 }
 
 TEST(test_utils, get_filename_not_found){
-    const char * path = "C:\\Users\\georges\\Downloads\\";
+    const char * path = PATH_TEST1;
     const char * filename = path_get_filename(path);
     CHECK(filename == NULL);
 }
@@ -59,11 +68,11 @@ TEST(test_utils, is_filename){
 }
 
 TEST(test_utils, path_concatenate){
-    const char * path1 = "C:\\Users\\georges\\Downloads\\";
-    const char * path11 = "C:\\Users\\georges\\Downloads";
+    const char * path1 = PATH_TEST1;
+    const char * path11 = PATH_TEST11;
     const char * path2 = "my-file.ext";
     const char * filename = path_concatenate(path1, path2);
-    STRCMP_EQUAL("C:\\Users\\georges\\Downloads\\my-file.ext", filename);
+    STRCMP_EQUAL(PATH_FULL, filename);
     const char * filename2 = path_concatenate(path11, path2);
-    STRCMP_EQUAL("C:\\Users\\georges\\Downloads\\my-file.ext", filename2);
+    STRCMP_EQUAL(PATH_FULL, filename2);
 }
